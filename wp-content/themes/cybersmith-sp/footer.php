a44aa6875a0826f0c43f89e438ce371b6ead1cc6
trainshogi <densyashogi@gmail.com>
2021-11-27 13:20:36 +0900

    add footer a link

--- wp-content/themes/cybersmith-sp/footer.php
+++ wp-content/themes/cybersmith-sp/footer.php
@@ -72,40 +72,44 @@
         </clipPath>
       </defs>
       <rect class="a5123f33-8205-431f-b08d-d7d190cbf406" x="-4.27" width="384.27" height="875.05"/>
+      <text class="b069c161-1fff-4566-8a26-07d6bacac7e9" transform="translate(37.78 208.53)">幸福が循環する<tspan x="43.2" y="63">社会を創る</tspan></text>
+      <text class="afd693b8-ae96-4d06-8ebf-5cfdcfb9200e" transform="translate(52.14 783.15)"><a href="../company_sp/">運営会社</a> / <a href=".">ご利用規約</a> / <a href="../qa_sp/">よくある質問</a></text>
+      <text class="adbd92b0-20fa-42fe-be7a-c2ae504bb931" transform="translate(37.54 817.97)">Copyright © Cybersmith All rights reserved.</text>
+      <text class="a8aca1e5-4334-48f5-8133-ba16f8742de4" transform="translate(93.31 114.2)">サイバースミス</text>
       <g class="ab4c8d90-07d0-48ce-bdf1-ebd7683de70d">
         <path d="M342.71,349.05V501.54h-305V349.05h305m7.63-7.63H30.1V509.17H350.34V341.42Z"/>
       </g>
-      <text class="b069c161-1fff-4566-8a26-07d6bacac7e9" transform="translate(37.78 208.53)">幸福が循環する<tspan x="43.2" y="63">社会を創る</tspan></text>
-      <text class="afd693b8-ae96-4d06-8ebf-5cfdcfb9200e" transform="translate(52.14 783.15)">運営会社 / ご利用規約 / よくある質問</text>
-      <text class="adbd92b0-20fa-42fe-be7a-c2ae504bb931" transform="translate(37.54 817.97)">Copyright © Cybersmith All rights reserved.</text>
-      <text class="a8aca1e5-4334-48f5-8133-ba16f8742de4" transform="translate(93.31 114.2)">サイバースミス</text>
       <g class="ab675e01-e4ba-4a70-bd67-c23c29cf2a04">
         <image width="1980" height="1320" transform="translate(-27.47 290.34) scale(0.2)" xlink:href="http://3.112.250.183/wp-content/uploads/2021/11/pixta_66330729_XL.jpg"/>
       </g>
-      <rect class="b1636575-22d1-4a73-affa-b1d330203faf" x="37.73" y="349.05" width="304.99" height="152.49"/>
-      <g>
-        <path class="f08c8953-3000-4181-a540-434569a2b540" d="M87.3,403.64a3.18,3.18,0,1,0,3.18,3.18A3.18,3.18,0,0,0,87.3,403.64Z"/>
-        <circle class="f08c8953-3000-4181-a540-434569a2b540" cx="96.32" cy="415.84" r="3.18"/>
-        <path class="f08c8953-3000-4181-a540-434569a2b540" d="M87.3,439.7a3.18,3.18,0,1,0,3.18,3.18A3.18,3.18,0,0,0,87.3,439.7Z"/>
-        <circle class="f08c8953-3000-4181-a540-434569a2b540" cx="96.32" cy="433.87" r="3.18"/>
-        <path class="f08c8953-3000-4181-a540-434569a2b540" d="M105.33,421.67a3.18,3.18,0,1,0,3.18,3.18A3.18,3.18,0,0,0,105.33,421.67Z"/>
-      </g>
-      <text class="b2643fe2-56b4-40ce-8a95-3ab9197b5c7e" transform="translate(151.32 433.92)">企業様へ</text>
       <g class="ab4c8d90-07d0-48ce-bdf1-ebd7683de70d">
         <path d="M344,550V702.48H39V550H344m7.62-7.62H31.39V710.11H351.62V542.37Z"/>
       </g>
       <g class="ac5aec5b-a6e5-4b2a-83a8-9245e9c20a36">
         <image width="1980" height="1320" transform="translate(29.86 527.37) scale(0.16)" xlink:href="http://3.112.250.183/wp-content/uploads/2021/11/pixta_63366991_XL.jpg"/>
       </g>
-      <rect class="b1636575-22d1-4a73-affa-b1d330203faf" x="39.01" y="549.99" width="304.99" height="152.49"/>
-      <g>
-        <path class="f08c8953-3000-4181-a540-434569a2b540" d="M88.3,615.64a3.18,3.18,0,1,0,3.18,3.18A3.18,3.18,0,0,0,88.3,615.64Z"/>
-        <circle class="f08c8953-3000-4181-a540-434569a2b540" cx="97.32" cy="627.84" r="3.18"/>
-        <path class="f08c8953-3000-4181-a540-434569a2b540" d="M88.3,651.7a3.18,3.18,0,1,0,3.18,3.18A3.18,3.18,0,0,0,88.3,651.7Z"/>
-        <circle class="f08c8953-3000-4181-a540-434569a2b540" cx="97.32" cy="645.87" r="3.18"/>
-        <path class="f08c8953-3000-4181-a540-434569a2b540" d="M106.33,633.67a3.18,3.18,0,1,0,3.18,3.18A3.18,3.18,0,0,0,106.33,633.67Z"/>
-      </g>
-      <text class="b2643fe2-56b4-40ce-8a95-3ab9197b5c7e" transform="translate(144.32 621.92)">お仕事を<tspan x="0" y="36.75">探している方へ</tspan></text>
+      <g><a href="../for_clients_sp/">
+        <rect class="b1636575-22d1-4a73-affa-b1d330203faf" x="37.73" y="349.05" width="304.99" height="152.49"/>
+        <g>
+          <path class="f08c8953-3000-4181-a540-434569a2b540" d="M87.3,403.64a3.18,3.18,0,1,0,3.18,3.18A3.18,3.18,0,0,0,87.3,403.64Z"/>
+          <circle class="f08c8953-3000-4181-a540-434569a2b540" cx="96.32" cy="415.84" r="3.18"/>
+          <path class="f08c8953-3000-4181-a540-434569a2b540" d="M87.3,439.7a3.18,3.18,0,1,0,3.18,3.18A3.18,3.18,0,0,0,87.3,439.7Z"/>
+          <circle class="f08c8953-3000-4181-a540-434569a2b540" cx="96.32" cy="433.87" r="3.18"/>
+          <path class="f08c8953-3000-4181-a540-434569a2b540" d="M105.33,421.67a3.18,3.18,0,1,0,3.18,3.18A3.18,3.18,0,0,0,105.33,421.67Z"/>
+        </g>
+        <text class="b2643fe2-56b4-40ce-8a95-3ab9197b5c7e" transform="translate(151.32 433.92)">企業様へ</text>
+      </a></g>
+      <g><a href="../for_worker_sp/">
+        <rect class="b1636575-22d1-4a73-affa-b1d330203faf" x="39.01" y="549.99" width="304.99" height="152.49"/>
+        <g>
+          <path class="f08c8953-3000-4181-a540-434569a2b540" d="M88.3,615.64a3.18,3.18,0,1,0,3.18,3.18A3.18,3.18,0,0,0,88.3,615.64Z"/>
+          <circle class="f08c8953-3000-4181-a540-434569a2b540" cx="97.32" cy="627.84" r="3.18"/>
+          <path class="f08c8953-3000-4181-a540-434569a2b540" d="M88.3,651.7a3.18,3.18,0,1,0,3.18,3.18A3.18,3.18,0,0,0,88.3,651.7Z"/>
+          <circle class="f08c8953-3000-4181-a540-434569a2b540" cx="97.32" cy="645.87" r="3.18"/>
+          <path class="f08c8953-3000-4181-a540-434569a2b540" d="M106.33,633.67a3.18,3.18,0,1,0,3.18,3.18A3.18,3.18,0,0,0,106.33,633.67Z"/>
+        </g>
+        <text class="b2643fe2-56b4-40ce-8a95-3ab9197b5c7e" transform="translate(144.32 621.92)">お仕事を<tspan x="0" y="36.75">探している方へ</tspan></text>
+      </a></g>
     </svg>
   </footer>
   <?php wp_footer(); ?>
