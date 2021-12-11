2c5243242ef673b57f9085575a6d79696528fbd3
trainshogi <densyashogi@gmail.com>
2021-12-11 13:53:50 +0900

    change svg to form

--- wp-content/page-pc/VD_work_detail-04.php
+++ wp-content/page-pc/VD_work_detail-04.php
@@ -1,122 +1,65 @@
-<svg id="fd8c5504-ddb2-41f6-932e-d2b3bea7f246" data-name="レイヤー 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1366 970">
+<svg id="fe5a18de-1040-4289-8670-97acbe2c54b1" data-name="レイヤー 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1366 167">
   <defs>
     <style>
-      .bc731e57-0315-4e8d-957a-974fb49e0fb0 {
-        fill: #e4e4e4;
-      }
-
-      .e932087f-e94d-4b4b-a24e-da4b448b9717, .fce698ae-0607-468b-9eb9-8f9497f9af6a {
-        font-size: 16px;
-      }
-
-      .aa8f9f55-513c-4703-b83f-e205a3c16c65, .e7d4ac5b-1ea3-4455-a974-5dac9d1214d3, .e932087f-e94d-4b4b-a24e-da4b448b9717, .f9952467-93ce-42cd-9ce1-3913e9347a2d {
-        font-family: HiraKakuProN-W3-83pv-RKSJ-H, Hiragino Kaku Gothic ProN;
-      }
-
-      .e932087f-e94d-4b4b-a24e-da4b448b9717 {
-        letter-spacing: 0.1em;
-      }
-
-      .a2c6683e-b509-4f5f-8115-a4b7dd0e8093 {
-        fill: #83ccd2;
-      }
-
-      .ec88a753-38be-4c4d-ba3b-3dc53c3b7968, .fce698ae-0607-468b-9eb9-8f9497f9af6a {
-        fill: #fff;
-      }
-
-      .fce698ae-0607-468b-9eb9-8f9497f9af6a {
-        font-family: HiraginoSans-W6-83pv-RKSJ-H, Hiragino Sans;
-        letter-spacing: 0.3em;
-      }
-
-      .f6a68195-a599-4522-9524-1202035679ac {
+      .aebd95c1-94db-49d8-b1af-68afce91c1a8 {
         font-size: 28px;
         font-family: FutoGoB101Pr6N-Bold, FutoGoB101Pr6N Bold-83pv-RKSJ-H;
         font-weight: 700;
         letter-spacing: 0.1em;
       }
 
-      .aa53f623-a00a-4c85-b092-42ba3a557e27, .b0bf78c9-e02c-4429-b562-da5e4dc34f33, .f5c593a5-bfe7-4756-8920-e8f34381687d {
+      .aae51d38-efe4-4f95-b29a-7a49fd4fd76d {
         fill: none;
+        stroke: #83ccd2;
         stroke-miterlimit: 10;
       }
 
-      .b0bf78c9-e02c-4429-b562-da5e4dc34f33 {
-        stroke: #83ccd2;
-      }
-
-      .f9952467-93ce-42cd-9ce1-3913e9347a2d {
+      .b8f11158-5736-461b-af08-53c583f970d7 {
         font-size: 18px;
+        font-family: HiraKakuProN-W3-83pv-RKSJ-H, Hiragino Kaku Gothic ProN;
         letter-spacing: 0.1em;
       }
 
-      .fdfea57b-e344-43b6-a06a-d80e17054b32 {
+      .a386178e-8a48-49a7-9e53-384fa88225c4 {
         letter-spacing: -0.4em;
       }
 
-      .fb57dfcd-637b-4f81-a229-cd96cf3eaab0 {
+      .a3be7aa6-a7b1-4e5a-991a-08c21931debb {
         letter-spacing: 0.35em;
       }
 
-      .aa8f9f55-513c-4703-b83f-e205a3c16c65, .baea776c-703c-4ca7-895d-347a710e1927, .e7d4ac5b-1ea3-4455-a974-5dac9d1214d3 {
+      .a31fe430-f28b-4531-ae1b-82efa55a8055 {
         letter-spacing: 0.1em;
-      }
-
-      .aa53f623-a00a-4c85-b092-42ba3a557e27, .f5c593a5-bfe7-4756-8920-e8f34381687d {
-        stroke: #e4e4e4;
-      }
-
-      .aa53f623-a00a-4c85-b092-42ba3a557e27 {
-        stroke-width: 3px;
-      }
-
-      .aa8f9f55-513c-4703-b83f-e205a3c16c65, .e7d4ac5b-1ea3-4455-a974-5dac9d1214d3 {
-        font-size: 12px;
-      }
-
-      .aa8f9f55-513c-4703-b83f-e205a3c16c65 {
-        fill: #bf272d;
       }
     </style>
   </defs>
-  <rect class="bc731e57-0315-4e8d-957a-974fb49e0fb0" x="226" y="249" width="200" height="65" rx="5.39"/>
-  <text class="e932087f-e94d-4b4b-a24e-da4b448b9717" transform="translate(240.61 276.44)">案件番号</text>
-  <g>
-    <rect class="a2c6683e-b509-4f5f-8115-a4b7dd0e8093" x="482.29" y="845.51" width="400" height="60" rx="30"/>
-    <g>
-      <circle class="a2c6683e-b509-4f5f-8115-a4b7dd0e8093" cx="844.7" cy="877.92" r="13.51"/>
-      <g>
-        <path class="ec88a753-38be-4c4d-ba3b-3dc53c3b7968" d="M846,878.14l0,0v0l0-.06h0a.38.38,0,0,0,0-.15h0s0,0,0-.06v0l0,0-.05-.06-5.41-5.42a.39.39,0,0,0-.55,0l-1,1a.43.43,0,0,0-.11.28.39.39,0,0,0,.11.27l4.11,4.11L838.9,882a.41.41,0,0,0,0,.56l1,1a.39.39,0,0,0,.27.11.43.43,0,0,0,.28-.11l5.41-5.42Z"/>
-        <path class="ec88a753-38be-4c4d-ba3b-3dc53c3b7968" d="M852,878.09v0l0-.06h0a.38.38,0,0,0,0-.15h0s0,0,0-.06v0l0,0,0-.06-5.42-5.42a.39.39,0,0,0-.55,0l-1,1a.39.39,0,0,0-.12.28.36.36,0,0,0,.12.27l4.11,4.11L844.88,882a.39.39,0,0,0,0,.56l1,1a.39.39,0,0,0,.27.11.43.43,0,0,0,.28-.11l5.42-5.42,0-.06Z"/>
-      </g>
-    </g>
-    <text class="fce698ae-0607-468b-9eb9-8f9497f9af6a" transform="translate(601.2 883.78)">送信（応募）する</text>
-  </g>
-  <text class="f6a68195-a599-4522-9524-1202035679ac" transform="translate(242.95 88.21)">本案件に応募する</text>
-  <line class="b0bf78c9-e02c-4429-b562-da5e4dc34f33" x1="214.85" y1="58.74" x2="182.85" y2="90.74"/>
-  <text class="f9952467-93ce-42cd-9ce1-3913e9347a2d" transform="translate(179.12 154.93)">本件にご関心のある方は下のフォームから履歴書・職務経歴書・スキルシートをお送りください<tspan class="fdfea57b-e344-43b6-a06a-d80e17054b32" x="851.4" y="0">。</tspan><tspan x="0" y="31.5">案件番号には、案件名の前に付されている番号（</tspan><tspan class="fb57dfcd-637b-4f81-a229-cd96cf3eaab0" x="435.6" y="31.5">#</tspan><tspan class="baea776c-703c-4ca7-895d-347a710e1927" x="453.73" y="31.5">に続く番号）の入力をお願いします</tspan><tspan class="fdfea57b-e344-43b6-a06a-d80e17054b32" x="770.53" y="31.5">。</tspan></text>
-  <rect class="aa53f623-a00a-4c85-b092-42ba3a557e27" x="448.5" y="258.5" width="600" height="45" rx="5.67"/>
-  <text class="aa8f9f55-513c-4703-b83f-e205a3c16c65" transform="translate(243.67 297.67)">※ 必須</text>
-  <rect class="bc731e57-0315-4e8d-957a-974fb49e0fb0" x="226" y="329" width="200" height="65" rx="5.39"/>
-  <text class="e932087f-e94d-4b4b-a24e-da4b448b9717" transform="translate(240.61 356.44)">氏名</text>
-  <rect class="bc731e57-0315-4e8d-957a-974fb49e0fb0" x="226" y="570" width="200" height="205" rx="9.57"/>
-  <text class="e932087f-e94d-4b4b-a24e-da4b448b9717" transform="translate(240.61 678.44)">任意メッセージ</text>
-  <rect class="aa53f623-a00a-4c85-b092-42ba3a557e27" x="448.5" y="338.5" width="600" height="45" rx="5.67"/>
-  <text class="aa8f9f55-513c-4703-b83f-e205a3c16c65" transform="translate(243.67 377.67)">※ 必須</text>
-  <rect class="bc731e57-0315-4e8d-957a-974fb49e0fb0" x="226" y="408" width="200" height="65" rx="5.39"/>
-  <text class="e932087f-e94d-4b4b-a24e-da4b448b9717" transform="translate(240.61 435.44)">メールアドレス</text>
-  <rect class="aa53f623-a00a-4c85-b092-42ba3a557e27" x="448.5" y="417.5" width="600" height="45" rx="5.67"/>
-  <rect class="aa53f623-a00a-4c85-b092-42ba3a557e27" x="448.5" y="572.5" width="600" height="200" rx="11.95"/>
-  <text class="aa8f9f55-513c-4703-b83f-e205a3c16c65" transform="translate(243.67 456.67)">※ 必須</text>
-  <rect class="bc731e57-0315-4e8d-957a-974fb49e0fb0" x="226" y="488" width="200" height="65" rx="5.39"/>
-  <text class="e932087f-e94d-4b4b-a24e-da4b448b9717" transform="translate(240.61 515.44)">スキルシートの添付</text>
-  <text class="aa8f9f55-513c-4703-b83f-e205a3c16c65" transform="translate(243.67 536.67)">※ 必須</text>
-  <rect class="f5c593a5-bfe7-4756-8920-e8f34381687d" x="455.5" y="509.5" width="155" height="28" rx="5"/>
-  <text class="e7d4ac5b-1ea3-4455-a974-5dac9d1214d3" transform="translate(486.85 528.06)">ファイルを選択</text>
-  <text class="e7d4ac5b-1ea3-4455-a974-5dac9d1214d3" transform="translate(627.85 528.06)">ファイル未選択</text>
-  <g>
-    <text class="f9952467-93ce-42cd-9ce1-3913e9347a2d" transform="translate(584.11 817.93)">個人情報の取扱いに同意する</text>
-    <rect class="aa53f623-a00a-4c85-b092-42ba3a557e27" x="524.49" y="797.5" width="36" height="28" rx="5"/>
-  </g>
+  <text class="aebd95c1-94db-49d8-b1af-68afce91c1a8" transform="translate(242.95 45.21)">本案件に応募する</text>
+  <line class="aae51d38-efe4-4f95-b29a-7a49fd4fd76d" x1="214.85" y1="15.74" x2="182.85" y2="47.74"/>
+  <text class="b8f11158-5736-461b-af08-53c583f970d7" transform="translate(179.12 111.93)">本件にご関心のある方は下のフォームから履歴書・職務経歴書・スキルシートをお送りください<tspan class="a386178e-8a48-49a7-9e53-384fa88225c4" x="851.4" y="0">。</tspan><tspan x="0" y="31.5">案件番号には、案件名の前に付されている番号（</tspan><tspan class="a3be7aa6-a7b1-4e5a-991a-08c21931debb" x="435.6" y="31.5">#</tspan><tspan class="a31fe430-f28b-4531-ae1b-82efa55a8055" x="453.73" y="31.5">に続く番号）の入力をお願いします</tspan><tspan class="a386178e-8a48-49a7-9e53-384fa88225c4" x="770.53" y="31.5">。</tspan></text>
 </svg>
+<form method="POST" action="" style="padding-top: 1vw; padding-bottom: 2vw; font-family: HiraginoSans-W6-83pv-RKSJ-H, Hiragino Sans;">
+  <table style="margin: 0 auto; letter-spacing: 0.1em; font-size: 1.17vw; font-family: HiraKakuProN-W3-83pv-RKSJ-H, Hiragino Kaku Gothic ProN;">
+    <tbody><tr>
+      <td style="vertical-align: middle;"><div style="margin: 0.3vw; background-color: #e4e4e4; padding: 0.5vw 1vw; width: 15.6vw; border-radius: 0.5vw;"><label>案件番号</label><br><span style="font-size: 0.88vw; color: #bf272d;">※ 必須</span></div></td>
+      <td><input type="text" style="height: 2.3vw; width: 40.8vw; margin: 0.7vw; font-size: 1.17vw; border: 0.2vw solid #e4e4e4; border-radius: 0.5vw;"></td>
+    </tr>
+    <tr>
+      <td style="vertical-align: middle;"><div style="margin: 0.3vw; background-color: #e4e4e4; padding: 0.5vw 1vw; width: 15.6vw; border-radius: 0.5vw;"><label>氏名</label><br><span style="font-size: 0.88vw; color: #bf272d;">※ 必須</span></div></td>
+      <td><input type="text" style="height: 2.3vw; width: 40.8vw; margin: 0.7vw; font-size: 1.17vw; border: 0.2vw solid #e4e4e4; border-radius: 0.5vw;"></td>
+    </tr>
+    <tr>
+      <td style="vertical-align: middle;"><div style="margin: 0.3vw; background-color: #e4e4e4; padding: 0.5vw 1vw; width: 15.6vw; border-radius: 0.5vw;"><label>メールアドレス</label><br><span style="font-size: 0.88vw; color: #bf272d;">※ 必須</span></div></td>
+      <td><input type="text" style="height: 2.3vw; width: 40.8vw; margin: 0.7vw; font-size: 1.17vw; border: 0.2vw solid #e4e4e4; border-radius: 0.5vw;"></td>
+    </tr>
+    <tr>
+      <td style="vertical-align: middle;"><div style="margin: 0.3vw; background-color: #e4e4e4; padding: 0.5vw 1vw; width: 15.6vw; border-radius: 0.5vw;"><label>スキルシートの添付</label><br><span style="font-size: 0.88vw; color: #bf272d;">※ 必須</span></div></td>
+      <td><input type="file" id="skillsheet" name="skillsheet" accept="application/pdf" style="font-size: 0.88vw; margin: 0.7vw;"></td>
+    </tr>
+    <tr>
+      <td style="vertical-align: middle;"><div style="margin: 0.3vw; background-color: #e4e4e4; padding: 4.5vw 1vw; width: 15.6vw; border-radius: 0.5vw;"><label>任意メッセージ</label></div></td>
+      <td><textarea style="height: 11.6vw; width: 40.8vw; margin: 0.7vw; font-size: 1.17vw; border: 0.2vw solid #e4e4e4; border-radius: 0.5vw;"></textarea></td>
+    </tr>
+  </tbody></table>
+  <div style="text-align: center; font-size: 1.31vw; margin: 0.5vw;"><label class="my-checkbox"><input type="checkbox" name="accept" value="accept" style="width: 2.6vw; height: 2.0vw; border: 0.2vw solid #e4e4e4; border-radius: 0.5vw;"><span class="checkmark"></span><a href="../privacy_policy/" style="margin-left: 2vw;">個人情報の取扱い</a>に同意する</label></div>
+  <div style="text-align: center;"><button type="submit" name="submit" value="submit" style="width: 29.2vw; height: 4.3vw; font-size: 1.17vw; letter-spacing: 0.3em; color: white; background-color: #83ccd2; border: 0; border-radius: 2.2vw;">送信(応募)する</button></div>
+</form>
